a0fc97b8b932430826f24a25d10a8d66dc16d857
Labeya <laurentmwn@gmail.com>
2025-11-24 00:21:56 +0100

    setup shadcn react and mode light,dark

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { ThemeToggle } from "@/components/themes/theme-toggle";
+
 export default function Home() {
   return (
+    <div>
     <h2>Welcome to Travel Management App</h2>
+    <ThemeToggle />
+    </div>
   );
 }
